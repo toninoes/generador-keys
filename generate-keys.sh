33be3c4ba2e07811c7bba76dea22efa39c4b2aba
Antonio Ruiz Rondán <toninoes@gmail.com>
2021-08-08 14:44:37 +0200

    Add files via upload

---
+++ generate-keys.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+LINEA="######################################################################"
+echo $LINEA
+echo "COMPROBANDO QUE ESTEN INSTALADOS PUTTYGEN Y PWGEN"
+if ! command -v puttygen &> /dev/null
+then
+    sudo apt-get install putty-tools -y
+fi
+
+if ! command -v pwgen &> /dev/null
+then
+    sudo apt-get install pwgen -y
+fi
+#sudo apt-get install putty-tools pwgen -y
+echo $LINEA
+
+if [ "$1" != "" ]; then
+	NOMBREKEY=$1
+	PASSPHRASE=$(pwgen 12 1 -sB)
+	echo "PASSPHRASE PARA ${NOMBREKEY}.pem y ${NOMBREKEY}.ppk"
+	echo $PASSPHRASE 2>&1 | tee $NOMBREKEY.passphrase.txt
+	echo $LINEA
+	ssh-keygen -P $PASSPHRASE -t rsa -b 2048 -m pem -f $NOMBREKEY.pem
+	puttygen $NOMBREKEY.pem -o $NOMBREKEY.ppk -O private --new-passphrase $NOMBREKEY.passphrase.txt --old-passphrase $NOMBREKEY.passphrase.txt
+else
+	echo "ERROR: Necesito nombre de key como argumento de este script."
+	echo "Ejemplo: ./generate-keys.sh fulanitokey"
+fi
+echo $LINEA
